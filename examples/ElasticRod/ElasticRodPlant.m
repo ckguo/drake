ecb58aea1337a1332a54e621166e0695be370fac
ckguo <courtneyguo96@gmail.com>
2015-02-17 16:48:21 -0500

    Update ElasticRodPlant.m

--- examples/ElasticRod/ElasticRodPlant.m
+++ examples/ElasticRod/ElasticRodPlant.m
@@ -1,5 +1,7 @@
 classdef ElasticRodPlant < DrakeSystem
-
+    %ELASTICRODPLANT Summary of this class goes here
+    %   Detailed explanation goes here
+    
     properties
     end
     
@@ -19,18 +21,26 @@ classdef ElasticRodPlant < DrakeSystem
       end
       
       function [utraj,xtraj]=rodTrajectory(obj)
-          x0 = zeros(3,1); 
-          xf = [1, 0.5, pi/2]';
-          tf0 = 3;
+          x0 = [1, 0, pi]'; 
+          xf = [0, 1.3, pi/2]';
+          tf0 = 4;
 
           N = 21;
           prog = DircolTrajectoryOptimization(obj,N,[2]);
           prog = prog.addStateConstraint(ConstantConstraint(x0),1);
           prog = prog.addStateConstraint(ConstantConstraint(xf),N);
+          Q = [[2, 0, 0]; [0, 2, 0]; [0, 0, 0]];
+          
+          x_0 = 0.5;
+          y_0 = 0.5;
+          r = 0.3;
+          viscircles([x_0, y_0], r);
+          
+          prog = prog.addStateConstraint(QuadraticConstraint(r^2-x_0^2-y_0^2, 100, Q, -2*[x_0, y_0, 0]'), [1:N]);
           prog = prog.addRunningCost(@cost);
           prog = prog.addFinalCost(@finalCost);
 
-          traj_init.x = PPTrajectory([0, 0, 0]);
+          traj_init.x = PPTrajectory(foh([0,tf0],[double(x0),double(xf)]));
 
           [xtraj,utraj,z,F,info] = prog.solveTraj(tf0,traj_init);
 
